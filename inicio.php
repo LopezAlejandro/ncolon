9fb39a8ef8a5c87d44bd1fbe0a7c3a5beebf2a6c
Alejandro Lopez <lopalejandro@gmail.com>
2024-05-27 13:52:19 -0300

    Modificacion de la grilla de contenido

--- inicio.php
+++ inicio.php
@@ -1,5 +1,5 @@
 <?php
-	include "header.php";
+include "header.php";
 ?>
 <div class="spacer d-none d-sm-none d-md-block"></div>
 <div class="spacer d-none d-sm-none d-md-block"></div>
@@ -7,179 +7,121 @@
 <div class="spacer d-none d-sm-none d-md-block"></div>
 <div class="spacer d-none d-sm-none d-md-block"></div>
 
-	<div id="lightgallery" class="container-fluid"> 
-		
-		<div class="row">
-			<div class="col-md-2"></div>
-         
-         <div class="col">
-				<a class="hvr-grow" data-download-url="false" data-responsive="img/375-1.jpg 375, img/480-1.jpg 480, img/1.jpg 800" data-src="img/1600-1.jpg" href=""><img class="img-fluid " src="img/thumb-1.jpg"></a>
-			</div>
-			
-			<div class='w-100 d-md-none'></div>		
-			
-			<div class="col">
-				<a class="hvr-grow" data-download-url="false" data-responsive="img/375-2.jpg 375, img/480-2.jpg 480, img/2.jpg 800" data-src="img/1600-2.jpg" href=""><img class="img-fluid" src="img/thumb-2.jpg"></a>
-			</div>
-			
-			<div class='w-100 d-md-none'></div>
+<div id="lightgallery" class="container-fluid">
 
-			<div class="col">
-				<a class="hvr-grow" data-download-url="false" data-responsive="img/375-3.jpg 375, img/480-3.jpg 480, img/3.jpg 800" data-src="img/1600-3.jpg" href=""><img class="img-fluid" src="img/thumb-3.jpg"></a>
-			</div>
+	<div class="row">
+		<div class="col-md-2"></div>
 
-			<div class='w-100 d-md-none'></div>
-			
-			<div class="col">
-				<a class="hvr-grow" data-download-url="false" data-responsive="img/375-4.jpg 375, img/480-4.jpg 480, img/4.jpg 800" data-src="img/1600-4.jpg" href=""><img class="img-fluid " src="img/thumb-4.jpg"></a>
-			</div>
-			
-			<div class='w-100 d-md-none'></div>			
-
-			<div class="col">
-				<a class="hvr-grow" data-download-url="false" data-responsive="img/375-5.jpg 375, img/480-5.jpg 480, img/5.jpg 800" data-src="img/1600-5.jpg" href=""><img class="img-fluid " src="img/thumb-5.jpg"></a>
-			</div>
-
-			<div class='w-100 d-md-none'></div>
-			
-			<div class="col">
-				<a class="hvr-grow" data-download-url="false" data-responsive="img/375-6.jpg 375, img/480-6.jpg 480, img/6.jpg 800" data-src="img/1600-6.jpg" href=""><img class="img-fluid " src="img/thumb-6.jpg"></a>
-			</div>
-			
-			<div class='w-100 d-md-none'></div>
-			
-			<div class="col">
-				<a class="hvr-grow" data-download-url="false" data-responsive="img/375-7.jpg 375, img/480-7.jpg 480, img/7.jpg 800" data-src="img/1600-7.jpg" href=""><img class="img-fluid " src="img/thumb-7.jpg"></a>
-			</div>
-			
-			<div class="col-md-1">
-			</div>
-
-			<div class='w-100'></div>
-			<div class="col-md-2 align-self-end d-none .d-sm-none d-md-block">
-				
-			</div>
-			<div class="col">
-				<a class="hvr-grow" data-download-url="false" data-responsive="img/375-8.jpg 375, img/480-8.jpg 480, img/8.jpg 800" data-src="img/1600-8.jpg" href=""><img class="img-fluid " src="img/thumb-8.jpg"></a>
-			</div>			
-			
-			<div class='w-100 d-md-none'></div>
-
-			<div class="col" style="background-color: #E7E1D3">
-				<a class="hvr-grow" data-download-url="false" data-responsive="img/375-9.jpg 375, img/480-9.jpg 480, img/9.jpg 800" data-src="img/1600-9.jpg" href=""><img class="img-fluid " src="img/thumb-9.jpg"></a>
-			</div>
-
-			<div class='w-100 d-md-none'></div>
-			
-			<div class="col">
-				<a class="hvr-grow" data-download-url="false" data-responsive="img/375-10.jpg 375, img/480-10.jpg 480, img/10.jpg 800" data-src="img/1600-10.jpg" href=""><img class="img-fluid " src="img/thumb-10.jpg"></a>
-			</div>
-
-			<div class='w-100 d-md-none'></div>
-			
-			<div class="col">
-				<a class="hvr-grow" data-download-url="false" data-responsive="img/375-11.jpg 375, img/480-11.jpg 480, img/11.jpg 800" data-src="img/1600-11.jpg" href=""><img class="img-fluid " src="img/thumb-11.jpg"></a>
-			</div>
-
-			<div class='w-100 d-md-none'></div>
-			
-			<div class="col">
-				<a class="hvr-grow" data-download-url="false" data-responsive="img/375-12.jpg 375, img/480-12.jpg 480, img/12.jpg 800" data-src="img/1600-12.jpg" href=""><img class="img-fluid " src="img/thumb-12.jpg"></a>
-			</div>
-
-			<div class='w-100 d-md-none'></div>
-			
-			<div class="col" style="background-color: #E7E1D3">
-				<a class="hvr-grow" data-download-url="false" data-responsive="img/375-13.jpg 375, img/480-13.jpg 480, img/13.jpg 800" data-src="img/1600-13.jpg" href=""><img class="img-fluid " src="img/thumb-13.jpg"></a>
-			</div>
-			
-			<div class='w-100 d-md-none'></div>			
-			
-			<div class="col">
-				<a class="hvr-grow" data-download-url="false" data-responsive="img/375-14.jpg 375, img/480-14.jpg 480, img/14.jpg 800" data-src="img/1600-14.jpg" href=""><img class="img-fluid " src="img/thumb-14.jpg"></a>
-			</div>	
-			
-			<div class="col-md-1">
-			</div>
-			
-			<div class='w-100'></div>
-			<div class="col-md-2 align-self-end d-none .d-sm-none d-md-block">
-					<!--<img class="img-fluid " src="img/head/obras.png">-->
-			</div>
-			<div class="col">
-				<a class="hvr-grow" data-download-url="false" data-responsive="img/375-15.jpg 375, img/480-15.jpg 480, img/15.jpg 800" data-src="img/1600-15.jpg" href=""><img class="img-fluid " src="img/thumb-15.jpg"></a>
-			</div>			
-			
-			<div class='w-100 d-md-none'></div>
-
-			<div class="col">
-				<a class="hvr-grow" data-download-url="false" data-responsive="img/375-16.jpg 375, img/480-16.jpg 480, img/16.jpg 800" data-src="img/1600-16.jpg" href=""><img class="img-fluid " src="img/thumb-16.jpg"></a>
-			</div>
-
-			<div class='w-100 d-md-none'></div>
-			
-			<div class="col">
-				<a class="hvr-grow" data-download-url="false" data-responsive="img/375-17.jpg 375, img/480-17.jpg 480, img/17.jpg 800" data-src="img/1600-17.jpg" href=""><img class="img-fluid " src="img/thumb-17.jpg"></a>
-			</div>
-
-			<div class='w-100 d-md-none'></div>
-			
-			<div class="col">
-				<a class="hvr-grow" data-download-url="false" data-responsive="img/375-18.jpg 375, img/480-18.jpg 480, img/18.jpg 800" data-src="img/1600-18.jpg" href=""><img class="img-fluid " src="img/thumb-18.jpg"></a>
-			</div>
-
-			<div class='w-100 d-md-none'></div>
-			
-			<div class="col">
-				<a class="hvr-grow" data-download-url="false" data-responsive="img/375-19.jpg 375, img/480-19.jpg 480, img/19.jpg 800" data-src="img/1600-19.jpg" href=""><img class="img-fluid " src="img/thumb-19.jpg"></a>
-			</div>
-
-			<div class='w-100 d-md-none'></div>
-			
-			<div class="col">
-				<a class="hvr-grow" data-download-url="false" data-responsive="img/375-20.jpg 375, img/480-20.jpg 480, img/20.jpg 800" data-src="img/1600-20.jpg" href=""><img class="img-fluid " src="img/thumb-20.jpg"></a>
-			</div>
-			
-			<div class='w-100 d-md-none'></div>			
-			
-			<div class="col">
-				<a class="hvr-grow" data-download-url="false" data-responsive="img/375-21.jpg 375, img/480-21.jpg 480, img/21.jpg 800" data-src="img/1600-21.jpg" href=""><img class="img-fluid " src="img/thumb-21.jpg"></a>
-			</div>
-			
-			<div class="col-md-1">
-			</div>
-			
-			<div class='w-100'></div>
-			<div class="col-md-3 align-self-end d-none .d-sm-none d-md-block">
-					<!--<img class="img-fluid " src="img/head/obras.png">-->
-			</div>
-	
+		<div class="col">
+			<a class="hvr-grow" data-download-url="false" data-responsive="img/375-2.jpg 375, img/480-2.jpg 480, img/2.jpg 800" data-src="img/1600-2.jpg" href=""><img class="img-fluid " src="img/thumb-2.jpg"></a>
 		</div>
-	
-</div>
-		
-		<div class="row d-block d-sm-block d-md-none"></div>
+
+		<div class='w-100 d-md-none'></div>
+
+		<div class="col">
+			<a class="hvr-grow" data-download-url="false" data-responsive="img/375-3.jpg 375, img/480-3.jpg 480, img/3.jpg 800" data-src="img/1600-3.jpg" href=""><img class="img-fluid" src="img/thumb-3.jpg"></a>
+		</div>
+
+		<div class='w-100 d-md-none'></div>
+
+		<div class="col">
+			<a class="hvr-grow" data-download-url="false" data-responsive="img/375-7.jpg 375, img/480-7.jpg 480, img/7.jpg 800" data-src="img/1600-7.jpg" href=""><img class="img-fluid" src="img/thumb-7.jpg"></a>
+		</div>
+
+		<div class='w-100 d-md-none'></div>
+
+		<div class="col">
+			<a class="hvr-grow" data-download-url="false" data-responsive="img/375-14.jpg 375, img/480-14.jpg 480, img/14.jpg 800" data-src="img/1600-14.jpg" href=""><img class="img-fluid " src="img/thumb-14.jpg"></a>
+		</div>
+
+		<div class='w-100 d-md-none'></div>
+
+		<div class="col">
+			<a class="hvr-grow" data-download-url="false" data-responsive="img/375-17.jpg 375, img/480-17.jpg 480, img/17.jpg 800" data-src="img/1600-17.jpg" href=""><img class="img-fluid " src="img/thumb-17.jpg"></a>
+		</div>
+
+		<div class='w-100 d-md-none'></div>
+
+
+		<div class="col-md-1"></div>
+
+		<div class='w-100'></div>
+
+		<div class="col-md-2 align-self-end d-none .d-sm-none d-md-block"></div>
+
+		<div class="col">
+			<a class="hvr-grow" data-download-url="false" data-responsive="img/375-20.jpg 375, img/480-20.jpg 480, img/20.jpg 800" data-src="img/1600-20.jpg" href=""><img class="img-fluid " src="img/thumb-20.jpg"></a>
+		</div>
+		<div class='w-100 d-md-none'></div>
+
+		<div class="col">
+			<a class="hvr-grow" data-download-url="false" data-responsive="img/375-23.jpg 375, img/480-23.jpg 480, img/23.jpg 800" data-src="img/1600-23.jpg" href=""><img class="img-fluid " src="img/thumb-23.jpg"></a>
+		</div>
+		<div class='w-100 d-md-none'></div>
+
+		<div class="col">
+			<a class="hvr-grow" data-download-url="false" data-responsive="img/375-24.jpg 375, img/480-24.jpg 480, img/24.jpg 800" data-src="img/1600-24.jpg" href=""><img class="img-fluid " src="img/thumb-24.jpg"></a>
+		</div>
+		<div class='w-100 d-md-none'></div>
+
+
+
+		<div class="col">
+			<a class="hvr-grow" data-download-url="false" data-responsive="img/375-28.jpg 375, img/480-28.jpg 480, img/28.jpg 800" data-src="img/1600-28.jpg" href=""><img class="img-fluid " src="img/thumb-28.jpg"></a>
+		</div>
+		<div class='w-100 d-md-none'></div>
+
+
+
+		<div class="col">
+			<a class="hvr-grow" data-download-url="false" data-responsive="img/375-37.jpg 375, img/480-37.jpg 480, img/37.jpg 800" data-src="img/1600-37.jpg" href=""><img class="img-fluid " src="img/thumb-37.jpg"></a>
+		</div>
+		<div class='w-100 d-md-none'></div>
+
+		<div class="col-md-1"></div>
+
+		<div class='w-100'></div>
+
+		<div class="col-md-5 align-self-end d-none .d-sm-none d-md-block"></div>
+
+		<div class="col">
+			<a class="hvr-grow" data-download-url="false" data-responsive="img/375-27.jpg 375, img/480-27.jpg 480, img/27.jpg 800" data-src="img/1600-27.jpg" href=""><img class="img-fluid " src="img/thumb-27.jpg"></a>
+		</div>
+		<div class='w-100 d-md-none'></div>
+
+		<div class="col">
+			<a class="hvr-grow" data-download-url="false" data-responsive="img/375-30.jpg 375, img/480-30.jpg 480, img/30.jpg 800" data-src="img/1600-30.jpg" href=""><img class="img-fluid " src="img/thumb-30.jpg"></a>
+		</div>
+		<div class='w-100 d-md-none'></div>
+
+		<div class="col-md-5"></div>
+
+
+
+
+
 	</div>
-	
-	<div class="spacer d-none d-sm-none d-md-block"></div>
-	<div class="spacer d-none d-sm-none d-md-block"></div>	
-	
-	<div class="col-md-3 align-self-end d-none d-sm-none d-md-block"></div>
-	
-	<div class="spacer d-none d-sm-none d-md-block"></div>
-	<div class="spacer d-none d-sm-none d-md-block"></div>
-	
-	<script type="text/javascript">
 
-	           $(document).ready(function() {
-	               $("#lightgallery").lightGallery({
-	                   thumbnail: true,
-	                   selector: "a"
-	               }); 
-	           });
+	<div class="row d-block d-sm-block d-md-none"></div>
+</div>
 
-	</script> 
-	
-	<?php
-	include "footer.php";
-	?>
+<div class="spacer d-none d-sm-none d-md-block"></div>
+<div class="spacer d-none d-sm-none d-md-block"></div>
+
+<div class="col-md-3 align-self-end d-none d-sm-none d-md-block"></div>
+
+<div class="spacer d-none d-sm-none d-md-block"></div>
+<div class="spacer d-none d-sm-none d-md-block"></div>
+
+<script type="text/javascript">
+	$(document).ready(function() {
+		$("#lightgallery").lightGallery({
+			thumbnail: true,
+			selector: "a"
+		});
+	});
+</script>
+
+<?php
+include "footer.php";
+?>
