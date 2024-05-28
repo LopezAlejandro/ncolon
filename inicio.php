6e162a0db41692993c44ed7b5730ffd57bede35a
Alejandro Lopez <lopalejandro@gmail.com>
2024-05-28 11:08:19 -0300

    cambio de la pagina ppal y del texto de intro

--- inicio.php
+++ inicio.php
@@ -7,95 +7,25 @@ include "header.php";
 <div class="spacer d-none d-sm-none d-md-block"></div>
 <div class="spacer d-none d-sm-none d-md-block"></div>
 
-<div id="lightgallery" class="container-fluid">
-	<div class="row">
-
-		<div class="col-md-2"></div>
-
-		<div class="col">
-			<a class="hvr-grow" data-download-url="false" data-responsive="img/375-2.jpg 375, img/480-2.jpg 480, img/2.jpg 800" data-src="img/1600-2.jpg" href=""><img class="img-fluid " src="img/thumb-2.jpg"></a>
-		</div>
-
-		<div class='w-100 d-md-none'></div>
-
-		<div class="col">
-			<a class="hvr-grow" data-download-url="false" data-responsive="img/375-3.jpg 375, img/480-3.jpg 480, img/3.jpg 800" data-src="img/1600-3.jpg" href=""><img class="img-fluid" src="img/thumb-3.jpg"></a>
-		</div>
-
-		<div class='w-100 d-md-none'></div>
-
-		<div class="col">
-			<a class="hvr-grow" data-download-url="false" data-responsive="img/375-7.jpg 375, img/480-7.jpg 480, img/7.jpg 800" data-src="img/1600-7.jpg" href=""><img class="img-fluid" src="img/thumb-7.jpg"></a>
-		</div>
-
-		<div class='w-100 d-md-none'></div>
-
-		<div class="col">
-			<a class="hvr-grow" data-download-url="false" data-responsive="img/375-14.jpg 375, img/480-14.jpg 480, img/14.jpg 800" data-src="img/1600-14.jpg" href=""><img class="img-fluid " src="img/thumb-14.jpg"></a>
-		</div>
-
-		<div class='w-100 d-md-none'></div>
-
-		<div class="col">
-			<a class="hvr-grow" data-download-url="false" data-responsive="img/375-17.jpg 375, img/480-17.jpg 480, img/17.jpg 800" data-src="img/1600-17.jpg" href=""><img class="img-fluid " src="img/thumb-17.jpg"></a>
-		</div>
-
-		<div class='w-100 d-md-none'></div>
-
-
-		<div class="col-md-1"></div>
-
-		<div class='w-100'></div>
-
-		<div class="col-md-2 align-self-end d-none .d-sm-none d-md-block"></div>
-
-		<div class="col">
-			<a class="hvr-grow" data-download-url="false" data-responsive="img/375-20.jpg 375, img/480-20.jpg 480, img/20.jpg 800" data-src="img/1600-20.jpg" href=""><img class="img-fluid " src="img/thumb-20.jpg"></a>
-		</div>
-		<div class='w-100 d-md-none'></div>
-
-		<div class="col">
-			<a class="hvr-grow" data-download-url="false" data-responsive="img/375-23.jpg 375, img/480-23.jpg 480, img/23.jpg 800" data-src="img/1600-23.jpg" href=""><img class="img-fluid " src="img/thumb-23.jpg"></a>
-		</div>
-		<div class='w-100 d-md-none'></div>
-
-		<div class="col">
-			<a class="hvr-grow" data-download-url="false" data-responsive="img/375-24.jpg 375, img/480-24.jpg 480, img/24.jpg 800" data-src="img/1600-24.jpg" href=""><img class="img-fluid " src="img/thumb-24.jpg"></a>
-		</div>
-		<div class='w-100 d-md-none'></div>
-
-		<div class="col">
-			<a class="hvr-grow" data-download-url="false" data-responsive="img/375-28.jpg 375, img/480-28.jpg 480, img/28.jpg 800" data-src="img/1600-28.jpg" href=""><img class="img-fluid " src="img/thumb-28.jpg"></a>
-		</div>
-		<div class='w-100 d-md-none'></div>
-
-
-
-		<div class="col">
-			<a class="hvr-grow" data-download-url="false" data-responsive="img/375-37.jpg 375, img/480-37.jpg 480, img/37.jpg 800" data-src="img/1600-37.jpg" href=""><img class="img-fluid " src="img/thumb-37.jpg"></a>
-		</div>
-		<div class='w-100 d-md-none'></div>
-
-		<div class="col-md-1"></div>
-
-		<div class='w-100'></div>
-
-		<div class="col-md-5 align-self-end d-none .d-sm-none d-md-block"></div>
-
-		<div class="col">
-			<a class="hvr-grow" data-download-url="false" data-responsive="img/375-27.jpg 375, img/480-27.jpg 480, img/27.jpg 800" data-src="img/1600-27.jpg" href=""><img class="img-fluid " src="img/thumb-27.jpg"></a>
-		</div>
-		<div class='w-100 d-md-none'></div>
-
-		<div class="col">
-			<a class="hvr-grow" data-download-url="false" data-responsive="img/375-30.jpg 375, img/480-30.jpg 480, img/30.jpg 800" data-src="img/1600-30.jpg" href=""><img class="img-fluid " src="img/thumb-30.jpg"></a>
-		</div>
-		<div class='w-100 d-md-none'></div>
-
-		<div class="col-md-5"></div>
+<div id="lightgallery" class="contenido">
+	<div class="im-der">
+		<a class="hvr-grow" data-download-url="false" data-responsive="img/375-27.jpg 375, img/480-27.jpg 480, img/27.jpg 800" data-src="img/1600-27.jpg" href=""><img class="img-fluid " src="img/thumb-27.jpg"></a>
 	</div>
-
-	<div class="row d-block d-sm-block d-md-none"></div>
+	<div class="im-izq">
+		<a class="hvr-grow" data-download-url="false" data-responsive="img/375-30.jpg 375, img/480-30.jpg 480, img/30.jpg 800" data-src="img/1600-30.jpg" href=""><img class="img-fluid " src="img/thumb-30.jpg"></a>
+	</div>
+	<div class="im-central">
+		<div class="im1"><a class="hvr-grow" data-download-url="false" data-responsive="img/375-2.jpg 375, img/480-2.jpg 480, img/2.jpg 800" data-src="img/1600-2.jpg" href=""><img class="img-fluid " src="img/thumb-2.jpg"></a></div>
+		<div class="im2"><a class="hvr-grow" data-download-url="false" data-responsive="img/375-3.jpg 375, img/480-3.jpg 480, img/3.jpg 800" data-src="img/1600-3.jpg" href=""><img class="img-fluid " src="img/thumb-3.jpg"></a></div>
+		<div class="im3"><a class="hvr-grow" data-download-url="false" data-responsive="img/375-7.jpg 375, img/480-7.jpg 480, img/7.jpg 800" data-src="img/1600-7.jpg" href=""><img class="img-fluid " src="img/thumb-7.jpg"></a></div>
+		<div class="im4"><a class="hvr-grow" data-download-url="false" data-responsive="img/375-14.jpg 375, img/480-14.jpg 480, img/14.jpg 800" data-src="img/1600-14.jpg" href=""><img class="img-fluid " src="img/thumb-14.jpg"></a></div>
+		<div class="im5"><a class="hvr-grow" data-download-url="false" data-responsive="img/375-17.jpg 375, img/480-17.jpg 480, img/17.jpg 800" data-src="img/1600-17.jpg" href=""><img class="img-fluid " src="img/thumb-17.jpg"></a></div>
+		<div class="im6"><a class="hvr-grow" data-download-url="false" data-responsive="img/375-20.jpg 375, img/480-20.jpg 480, img/20.jpg 800" data-src="img/1600-20.jpg" href=""><img class="img-fluid " src="img/thumb-20.jpg"></a></div>
+		<div class="im7"><a class="hvr-grow" data-download-url="false" data-responsive="img/375-23.jpg 375, img/480-23.jpg 480, img/23.jpg 800" data-src="img/1600-23.jpg" href=""><img class="img-fluid " src="img/thumb-23.jpg"></a></div>
+		<div class="im8"><a class="hvr-grow" data-download-url="false" data-responsive="img/375-24.jpg 375, img/480-24.jpg 480, img/24.jpg 800" data-src="img/1600-24.jpg" href=""><img class="img-fluid " src="img/thumb-24.jpg"></a></div>
+		<div class="im9"><a class="hvr-grow" data-download-url="false" data-responsive="img/375-28.jpg 375, img/480-28.jpg 480, img/28.jpg 800" data-src="img/1600-28.jpg" href=""><img class="img-fluid " src="img/thumb-28.jpg"></a></div>
+		<div class="im10"><a class="hvr-grow" data-download-url="false" data-responsive="img/375-37.jpg 375, img/480-37.jpg 480, img/37.jpg 800" data-src="img/1600-37.jpg" href=""><img class="img-fluid " src="img/thumb-37.jpg"></a></div>
+	</div>
 </div>
 
 <!-- <div class="spacer d-none d-sm-none d-md-block"></div> -->
--- intro.php
+++ intro.php
@@ -16,36 +16,16 @@ include "header1.php";
 
 			<div class="two-columns">
 				<p class="preludio">
-					La historia del Teatro Colón es rica y compleja, involucrando varios arquitectos a lo largo de su
-					construcción. Su construcción comenzó en 1889.<br>
-					El arquitecto original fue Francesco Tamburini, un italiano que diseñó el proyecto inicial con un
-					estilo ecléctico. Tras su muerte en 1890, Víctor Meano, también arquitecto italiano, asumió la
-					dirección del proyecto. Meano respetó el diseño inicial de Tamburini pero introdujo sus propias
-					modificaciones y detalles arquitectónicos.<br>
-					Meano aportó una mezcla de estilos renacentista italiano y neoclásico, y trabajó en el teatro hasta
-					su muerte en 1904.<br>
-					La construcción fue finalmente completada por el arquitecto belga Jules Dormal, quien añadió
-					elementos del Art Nouveau y contribuyó con los últimos toques.<br>
-					El Teatro Colón se inauguró el 25 de mayo de 1908, después de casi 20 años de construcción.<br>
-					En la publicación "El Nuevo Teatro Colón, Buenos Aires...: planos definitivos aprobados por el
-					Superior Gobierno con Decreto de fecha 10 de septiembre de 1892: especificaciones, detalles de
-					construcción y de ornamentación", Víctor Meano ofrece una visión detallada del proyecto del Teatro
-					Colón aprobado en 1892. Esta obra, publicada por la editorial Kraft, incluye una descripción
-					exhaustiva de los aspectos arquitectónicos y funcionales del teatro.<br>
-					Meano inicia con un resumen histórico de la gestión del proyecto hasta 1892, rindiendo homenaje a su
-					predecesor, Francesco Tamburini, y reconociendo a los colaboradores que participaron en el
-					desarrollo del teatro. En el texto, Meano detalla las modificaciones introducidas en el diseño
-					original, como la nueva resolución volumétrica que integra la sala principal y el palco escénico
-					bajo una misma cubierta, formando una estructura prismática distintiva.<br>
-					Además, el documento explica cómo se han dispuesto las comodidades para el público, dividiendo los
-					espacios en dos grandes grupos: el de los asistentes al "paraíso" y el de los que frecuentan la
-					platea, palco, cazuela y galería. Se describen las áreas de vestíbulos, salones, foyer y escaleras,
-					enfatizando su importancia para el confort y la experiencia del público durante los intermedios y al
-					finalizar las funciones.<br>
-					La publicación incluye 39 láminas que contienen los planos del teatro, con ilustraciones, plantas,
-					cortes y vistas que permiten una comprensión visual completa del proyecto arquitectónico. Esta obra
-					se encuentra digitalizada y es parte de la "Colección de Libros Antiguos y Valiosos" de la
-					biblioteca y se presenta en una exposición virtual que destaca una selección de dichas láminas.
+					Víctor Meano fue un arquitecto italiano cuya visión y talento dejaron una marca indeleble en la arquitectura argentina. Nacido en Susa, Italia, en 1860, Meano estudió arquitectura en el Politécnico de Turín, una de las instituciones más prestigiosas de Europa. Su formación en Italia, cuna de la arquitectura renacentista y barroca influyó significativamente en su estilo y en su enfoque detallista y ornamentado. Emigró a Argentina en la década de 1880. Rápidamente se integró en la sociedad arquitectónica de Buenos Aires, donde sus habilidades y formación europea lo destacaron entre sus contemporáneos. <br>
+					La historia del Teatro Colón es rica y compleja, involucrando varios arquitectos a lo largo de su construcción. Su construcción comenzó en 1889. La tarea de diseñar un nuevo teatro de ópera para Buenos Aires recayó inicialmente en el arquitecto Francesco Tamburini. Tras la muerte de Tamburini en 1890, Víctor Meano, quien había trabajado como su asistente, se encargó del proyecto. Aportó una mezcla de estilos renacentista italiano y neoclásico, y trabajó en el teatro hasta su muerte en 1904. La construcción fue finalmente completada por el arquitecto belga Jules Dormal, quien añadió elementos del Art Nouveau y contribuyó con los últimos toques.<br>
+					Víctor Meano no vivió para ver la culminación de su obra maestra, sin embargo, su visión y su talento quedaron inmortalizados en el Teatro Colón, que fue finalmente inaugurado el 25 de mayo de 1908, después de casi 20 años de construcción. Su diseño ha sido aclamado por su excepcional acústica y su impresionante estética, convirtiéndose en un símbolo perdurable del esplendor cultural de Buenos Aires. <br>
+					Los planos definitivos de Víctor Meano para el Teatro Colón fueron aprobados por el Superior Gobierno de Argentina con un decreto fechado el 10 de septiembre de 1892. Este decreto no solo sancionó el diseño de Meano, sino que también confirmó el compromiso del gobierno con la construcción de un teatro que se convertiría en un símbolo de la cultura y la modernidad de Buenos Aires. <br>
+					En la obra <em>"El Nuevo Teatro Colón, Buenos Aires...: planos definitivos aprobados por el Superior Gobierno con Decreto de fecha 10 de septiembre de 1892: especificaciones, detalles de construcción y de ornamentación"</em>, Víctor Meano ofrece una visión detallada del proyecto del Teatro Colón aprobado en 1892. Esta obra, publicada por la editorial Kraft, incluye una descripción meticulosa de los aspectos arquitectónicos y funcionales del teatro. <br>
+					Meano inicia con un resumen histórico de la gestión del proyecto hasta 1892, rindiendo homenaje a su predecesor, Francesco Tamburini, y reconociendo a los colaboradores que participaron en el desarrollo del teatro. En el texto, Meano detalla las modificaciones introducidas en el diseño original, como la nueva resolución volumétrica que integra la sala principal y el palco escénico bajo una misma cubierta, formando una estructura prismática distintiva. <br>
+					Además, el documento explica la disposición de los espacios, la organización interna del teatro, incluyendo la sala principal, el foyer, las escaleras, y las áreas técnicas y de servicio, enfatizando su importancia para el confort y la experiencia del público. <br>
+					La elección de materiales nobles como el mármol y las maderas finas, que contribuirían a la elegancia y durabilidad del edificio. Detalla los elementos decorativos, desde las molduras y frisos hasta los chandeliers y esculturas, que aportarían al teatro su carácter lujoso y distintivo y las innovaciones técnicas, los sistemas de tramoya y maquinaria escénica, diseñados para facilitar la producción de espectáculos complejos y de gran escala. <br>
+					La publicación incluye 39 láminas que contienen los planos del teatro, con ilustraciones, plantas, cortes y vistas que permiten una comprensión visual completa del proyecto arquitectónico.<br>
+					Esta obra se encuentra digitalizada y es parte de la <strong>"Colección de Libros Antiguos y Valiosos"</strong> de la biblioteca y se presenta en una exposición virtual que destaca una selección de dichas láminas.
 				</p>
 			</div>
 
